0b2d492713164de212659eed3d3d08b8ac549e54
Christian Oestreich <acetrike@yahoo.com>
2011-09-01 17:10:23 -0500

    Adding todo for getting autowire of client factory setup

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,13 +1,11 @@
-import com.grails.cxf.client.WebServiceClientFactoryImpl
 import org.codehaus.groovy.grails.commons.ConfigurationHolder
 
 // Place your Spring DSL code here
 beans = {
 
-    //webServiceClientFactory(WebServiceClientFactoryImpl)
-
     simpleServiceClient(com.grails.cxf.client.DynamicWebServiceClient) {
-        //webServiceClientFactory = ref("webServiceClientFactory")
+        //todo: need to get this working with auto injection from the plugin
+        webServiceClientFactory = ref("webServiceClientFactory")
         clientInterface = cxf.client.demo.simple.SimpleServicePortType
         serviceName = "simpleServiceClient"
         serviceEndpointAddress = ConfigurationHolder.config.service.simple.url
@@ -15,7 +13,8 @@ beans = {
     }
 
     complexServiceClient(com.grails.cxf.client.DynamicWebServiceClient) {
-        //webServiceClientFactory = ref("webServiceClientFactory")
+        //todo: need to get this working with auto injection from the plugin
+        webServiceClientFactory = ref("webServiceClientFactory")
         clientInterface = cxf.client.demo.complex.ComplexServicePortType
         serviceName = "complexServiceClient"
         serviceEndpointAddress = ConfigurationHolder.config.service.complex.url
