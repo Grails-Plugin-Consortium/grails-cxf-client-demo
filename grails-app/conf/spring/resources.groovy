43857e61a19eb90e5aef37dbd9ed0a93eb03d17b
Christian Oestreich <acetrike@yahoo.com>
2011-09-06 14:52:48 -0500

    Cleaning up plugin to use config.groovy now

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -3,21 +3,4 @@ import org.codehaus.groovy.grails.commons.ConfigurationHolder
 // Place your Spring DSL code here
 beans = {
 
-//    simpleServiceClient(com.grails.cxf.client.DynamicWebServiceClient) {
-//        //todo: need to get this working with auto injection from the plugin
-//        webServiceClientFactory = ref("webServiceClientFactory")
-//        clientInterface = cxf.client.demo.simple.SimpleServicePortType
-//        serviceName = "simpleServiceClient"
-//        serviceEndpointAddress = ConfigurationHolder.config.service.simple.url
-//        secured = false
-//    }
-//
-//    complexServiceClient(com.grails.cxf.client.DynamicWebServiceClient) {
-//        //todo: need to get this working with auto injection from the plugin
-//        webServiceClientFactory = ref("webServiceClientFactory")
-//        clientInterface = cxf.client.demo.complex.ComplexServicePortType
-//        serviceName = "complexServiceClient"
-//        serviceEndpointAddress = ConfigurationHolder.config.service.complex.url
-//        secured = false
-//    }
 }
