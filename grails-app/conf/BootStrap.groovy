1d8f1ac5012f8ab50f1c6c920c736db466d81ff3
Christian Oestreich <acetrike@yahoo.com>
2012-04-06 17:41:35 -0500

    successfully was able to wire in simple wss4j user/plainpw for new 1.6 version.  must use a Validator now to perform this function.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,21 +1,16 @@
 import org.apache.cxf.ws.security.wss4j.WSS4JInInterceptor
 import org.apache.ws.security.WSConstants
-import org.apache.ws.security.WSPasswordCallback
 import org.apache.ws.security.WSSecurityEngine
 import org.apache.ws.security.WSSecurityException
 import org.apache.ws.security.handler.WSHandlerConstants
 import org.apache.ws.security.validate.UsernameTokenValidator
 import org.apache.ws.security.validate.Validator
 
-import javax.security.auth.callback.Callback
-import javax.security.auth.callback.CallbackHandler
-import javax.security.auth.callback.UnsupportedCallbackException
 import javax.xml.namespace.QName
 
 class BootStrap {
 
     def secureServiceFactory
-    def authInterceptor
 
     def init = { servletContext ->
 
@@ -35,18 +30,6 @@ class BootStrap {
             }
         });
         inProps.put(WSS4JInInterceptor.VALIDATOR_MAP, validatorMap);
-        inProps.put(WSHandlerConstants.PW_CALLBACK_REF, new CallbackHandler() {
-
-            void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
-                WSPasswordCallback pc = (WSPasswordCallback) callbacks[0]
-                println pc.identifier
-                println pc.password
-                if(pc.identifier == "wsuser" && pc.password != "secret") {
-                    println "error :: wrong password"
-                    throw new IOException("wrong password")
-                }
-            }
-        })
         secureServiceFactory.getInInterceptors().add(new WSS4JInInterceptor(inProps))
     }
 
