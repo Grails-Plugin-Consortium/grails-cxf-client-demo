4bc0ed229fdc78508c725cbc773cd24be542684a
Christian Oestreich <acetrike@yahoo.com>
2013-09-18 09:41:42 -0500

    Testing with grails 2.3.0

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -3,12 +3,7 @@ grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.dependency.resolution = {
 
-    def exportLibs = { export: false }
-    def excludeConflicting = { excludes 'xmlbeans', 'spring-web', 'spring-core', 'xml-apis' }
-    def pluginsGroup = 'org.grails.plugins'
-    def grailsVersion = '2.1.3'
-
-    inherits("global") { }
+    inherits("global") {}
     log "info" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
 
     repositories {
@@ -24,24 +19,19 @@ grails.project.dependency.resolution = {
 
     plugins {
 
-        runtime name: 'hibernate',
-                version: grailsVersion,
-                group: pluginsGroup,
-                exportLibs
+        compile ":hibernate4:4.1.11.1"
+        build ':tomcat:7.0.41'
 
-        runtime name: 'tomcat',
-                version: grailsVersion,
-                group: pluginsGroup,
-                exportLibs
+        compile ":jasper:1.6.1"
 
         compile ":cxf:1.1.1"
         compile ":cxf-client:1.5.6"
-        compile ":spring-security-core:1.2.7.3"
-        compild ":spring-security-acl:1.1.1"
+//        compile ":spring-security-core:1.2.7.3"
+//        compild ":spring-security-acl:1.1.1"
 
-        test ":spock:0.6"
-        test ":code-coverage:1.2.5"
-        test ":codenarc:0.17"
+//        test ":spock:0.6"
+//        test ":code-coverage:1.2.5"
+//        test ":codenarc:0.17"
     }
 }
 
