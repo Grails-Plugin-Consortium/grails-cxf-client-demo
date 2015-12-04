cfcabad2aee4c670d43af21051195f7d585f6a59
christian.oestreich <christian.oestreich@bluestembrands.com>
2015-12-04 13:45:08 -0600

    Regenerating the services to make sure the script works

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -3,13 +3,12 @@ import com.cxf.demo.logging.CustomLoggingInInterceptor
 import com.cxf.demo.logging.CustomLoggingOutInterceptor
 import com.cxf.demo.logging.VerboseCustomLoggingInInterceptor
 import com.cxf.demo.security.CustomSecurityInterceptor
-import com.grails.cxf.client.security.DefaultSecurityOutInterceptor
+import org.grails.cxf.client.security.DefaultSecurityOutInterceptor
 import org.apache.cxf.configuration.security.AuthorizationPolicy
 import org.apache.cxf.transports.http.configuration.HTTPClientPolicy
 
 // Place your Spring DSL code here
 beans = {
-
 
 	myCustomInterceptor(CustomSecurityInterceptor)
 
